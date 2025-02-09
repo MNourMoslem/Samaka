95765f3864aad21759d6c978a2cce8f9131d1722
= <normoslem256@gmail.com>
2025-02-09 22:37:22 +0300

    Replace chess library with nchess and update board_to_tensor function for compatibility

--- samaka/tools.py
+++ samaka/tools.py
@@ -1,32 +1,5 @@
-import chess
+import nchess as nc
 import torch
 
-if chess.WHITE == 1 and chess.BLACK == 0:
-    get_color = lambda x: x
-else:
-    color_to_num = {
-        chess.WHITE: 1,
-        chess.BLACK: 0,
-    }
-    get_color = lambda x: color_to_num[x]
-
-piece_to_number = {
-    chess.PAWN: 1,
-    chess.KNIGHT: 2,
-    chess.BISHOP: 3,
-    chess.ROOK: 4,
-    chess.QUEEN: 5,
-    chess.KING: 6,
-}
-
-def board_to_tensor(board, device = None):
-    tensor = torch.zeros(64, dtype=torch.int64, device=device)
-
-    for square in chess.SQUARES:
-        piece = board.piece_at(square)
-        if piece:
-            base_number = piece_to_number[piece.piece_type]
-            color_offset = 6 * get_color(piece.color)
-            tensor[square] = base_number + color_offset
-
-    return tensor
+def board_to_tensor(board : nc.Board, device = None):
+    return torch.from_numpy(board.as_array(), dtype = torch.float32, device = device)
